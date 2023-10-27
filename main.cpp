912a95a90285f4b556ba4d396dfa8df731e0af41
Timofey <76160386+TEAmofey@users.noreply.github.com>
2023-10-28 00:29:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main() {
     std::vector<std::string> ans;
     std::vector<int> most_posible_buckets = {976369, 902483, 834181, 771049, 712697,
                                              658753, 608903, 562841, 520241};
-    std::string filename = "/data/aboba";
+    std::string filename = "/home/admin/data/aboba";
     for (int i = 0; i < most_posible_buckets.size(); i++) {
         count = 0;
         std::fstream fout(filename + std::to_string(i));
