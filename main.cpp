b87d68f729e36eff2e232fbff6e6da1a2c180361
Timofey <76160386+TEAmofey@users.noreply.github.com>
2023-10-28 00:36:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,11 +19,11 @@ int main() {
     std::vector<std::string> ans;
     std::vector<int> most_posible_buckets = {976369, 902483, 834181, 771049, 712697,
                                              658753, 608903, 562841, 520241};
-    std::string filename = "/home/admin/data/aboba";
+    std::string filename = "../data/aboba";
     for (int i = 0; i < most_posible_buckets.size(); i++) {
         count = 0;
         std::fstream fout(filename + std::to_string(i));
-        while (count < 15) {
+        while (count < 15'000) {
             for (int j = 0; j < 15; j++) {
                 str[j] = gen() % 26 + 'a';
             }
