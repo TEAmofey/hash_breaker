65ba07b88207c6db4d073689c4ff03c26ac14e2b
Timofey <76160386+TEAmofey@users.noreply.github.com>
2023-10-29 19:38:59 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,28 +10,44 @@
 std::random_device rd;
 std::mt19937 gen(rd());
 
+void next(std::string& s) {
+    for(int i = s.size(); i >= 0; --i) {
+        if(s[i] == 'z') {
+            s[i] = 'A';
+            break;
+        } else if (s[i] == 'Z') {
+            s[i] = '0';
+            break;
+        } else if (s[i] == '9') {
+            s[i] = '_';
+            break;
+        } else if (s[i] == '_') {
+            s[i] = 'a';
+        } else {
+            s[i] += 1;
+            break;
+        }
+    }
+}
+
 int main() {
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
     std::hash<std::string> h;
-    char str[15];
+    std::string str(15, 'a');
     int count;
     std::vector<std::string> ans;
-    std::vector<int> most_posible_buckets = {976369, 902483, 834181, 771049, 712697,
-                                             658753, 608903, 562841, 520241};
-    std::string filename = "../data/aboba";
+    int posible_bucket = 976369;
+    std::string filename = "../data/aboba0";
     for (int i = 0; i < most_posible_buckets.size(); i++) {
         count = 0;
         std::fstream fout(filename + std::to_string(i));
-        while (count < 15'000) {
-            for (int j = 0; j < 15; j++) {
-                str[j] = gen() % 26 + 'a';
-            }
-            auto aboba = std::string(str, 15);
-            if (h(aboba) % most_posible_buckets[i] == most_posible_buckets[i] - 1) {
+        while (count < 150) {
+            if (h(str) % most_posible_buckets[i] == most_posible_buckets[i] - 1) {
                 fout << aboba << '\n';
                 count++;
             }
+            next(str);
         }
     }
 }
